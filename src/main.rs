05a30f02b86dd13a3a7bd5d969614043e4341281
shatalov.ie <shatalov.ie@outlook.com>
2026-03-22 22:09:53 +0300

    boi micro rework

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,182 @@
+use log::warn;
+use std::error::Error;
 use teloxide::{
     dispatching::dialogue::InMemStorage,
+    payloads::SendMessageSetters,
     prelude::*,
-    types::{InlineKeyboardButton, InlineKeyboardButtonKind, InlineKeyboardMarkup},
+    sugar::bot::BotMessagesExt,
+    types::{
+        InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
+        InputMessageContentText, KeyboardButton, KeyboardMarkup, Me,
+    },
+    utils::command::BotCommands,
 };
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
-
-pub enum Language {
-    Russian,
-    Belorussian,
-    English,
-}
 
 #[derive(Clone, Default)]
 pub enum State {
     #[default]
     Start,
     RecieveLanguage,
-    ReceiveAge {
-        full_name: String,
+    RecieveStyle {
+        language: String,
     },
-    ReceiveLocation {
-        full_name: String,
-        age: u8,
-    },
+    WaitForTask,
 }
 
+/// These commands are supported:
+#[derive(BotCommands)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    /// Display this text
+    Help,
+    /// Start
+    Start,
+}
+
+const SUPPORTED_LANGUAGES: [&str; 3] = ["Русский", "English", "Chineese"];
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
-    log::info!("Starting dialogue bot...");
+    log::info!("Starting buttons bot...");
 
     let bot = Bot::from_env();
 
-    Dispatcher::builder(
-        bot,
-        Update::filter_message()
-            .enter_dialogue::<Message, InMemStorage<State>, State>()
-            .branch(dptree::case![State::Start].endpoint(start))
-            .branch(dptree::case![State::RecieveLanguage].endpoint(recieve_language))
-            .branch(dptree::case![State::ReceiveAge { full_name }].endpoint(receive_age))
-            .branch(
-                dptree::case![State::ReceiveLocation { full_name, age }].endpoint(receive_location),
-            ),
-    )
-    .dependencies(dptree::deps![InMemStorage::<State>::new()])
-    .enable_ctrlc_handler()
-    .build()
-    .dispatch()
-    .await;
+    let handler = dptree::entry()
+        .branch(
+            Update::filter_message()
+                .enter_dialogue::<Message, InMemStorage<State>, State>()
+                .branch(dptree::case![State::Start].endpoint(start))
+                .branch(dptree::case![State::RecieveLanguage].endpoint(receive_lang))
+                .branch(dptree::case![State::RecieveStyle { language }].endpoint(receive_style)),
+        )
+        .branch(Update::filter_callback_query().endpoint(callback_handler))
+        .branch(Update::filter_inline_query().endpoint(inline_query_handler));
+
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+    Ok(())
 }
 
-fn new_button(visible_string: &str, callback_string: &str) -> InlineKeyboardButton {
-    InlineKeyboardButton::new(
-        visible_string,
-        InlineKeyboardButtonKind::CallbackData(callback_string.to_string()),
-    )
+/// Creates a keyboard made by buttons in a big column.
+fn make_inline_keyboard(vector: Vec<&str>, chunks: usize) -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    for elements in vector.chunks(chunks) {
+        let row = elements
+            .iter()
+            .map(|&element| InlineKeyboardButton::callback(element.to_owned(), element.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
+fn make_keyboard(vector: Vec<&str>, chunks: usize) -> KeyboardMarkup {
+    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
+
+    for elements in vector.chunks(chunks) {
+        let row = elements
+            .iter()
+            .map(|&element| KeyboardButton::new(element.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    KeyboardMarkup::new(keyboard)
+}
+
+/// Parse the text wrote on Telegram and check if that text is a valid command
+/// or not, then match the command. If the command is `/start` it writes a
+/// markup with the `InlineKeyboardMarkup`.
+async fn message_handler(
+    bot: Bot,
+    msg: Message,
+    me: Me,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(text) = msg.text() {
+        match BotCommands::parse(text, me.username()) {
+            Ok(Command::Help) => {
+                // Just send the description of all commands.
+                bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                    .await?;
+            }
+            Ok(Command::Start) => {
+                // Create a list of buttons and send them.
+                // Создание клавиатуры пример
+                let keyboard =
+                    KeyboardMarkup::new(vec![vec![KeyboardButton::new("alo")]]).resize_keyboard();
+                bot.send_message(msg.chat.id, "Debian versions:")
+                    .reply_markup(keyboard)
+                    .await?;
+            }
+
+            Err(_) => {
+                bot.send_message(msg.chat.id, "Command not found!").await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn inline_query_handler(
+    bot: Bot,
+    q: InlineQuery,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let choose_debian_version = InlineQueryResultArticle::new(
+        "0",
+        "Chose debian version",
+        InputMessageContent::Text(InputMessageContentText::new("Debian versions:")),
+    );
+    //.reply_markup(make_inline_keyboardkeyboard());
+
+    bot.answer_inline_query(q.id, vec![choose_debian_version.into()])
+        .await?;
+
+    Ok(())
+}
+
+/// When it receives a callback from a button it edits the message with all
+/// those buttons writing a text with the selected Debian version.
+///
+/// **IMPORTANT**: do not send privacy-sensitive data this way!!!
+/// Anyone can read data stored in the callback button.
+async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(version) = q.data.as_ref() {
+        let text = format!("You chose: {version}");
+
+        // Tell telegram that we've seen this query, to remove 🕑 icons from the
+        // clients. You could also use `answer_callback_query`'s optional
+        // parameters to tweak what happens on the client side.
+        bot.answer_callback_query(q.id.clone()).await?;
+
+        // Edit text of the message to which the buttons were attached
+        if let Some(message) = q.regular_message() {
+            bot.edit_text(message, text).await?;
+        } else if let Some(id) = q.inline_message_id {
+            bot.edit_message_text_inline(id, text).await?;
+        }
+
+        log::info!("You chose: {version}");
+    }
+
+    Ok(())
 }
 
 async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-    // Create a keyboard markup with a single row containing the button
-    let keyboard = InlineKeyboardMarkup::new(vec![vec![new_button("Беларускі 🇧🇾", "Belarus")]]);
-
+    let keyboard =
+        KeyboardMarkup::new(vec![vec![KeyboardButton::new("Русский")]]).resize_keyboard();
     bot.send_message(msg.chat.id, "🗣️❓")
         .reply_markup(keyboard)
         .await?;
@@ -70,13 +184,13 @@ async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
     Ok(())
 }
 
-async fn recieve_language(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+async fn receive_lang(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
     match msg.text() {
         Some(text) => {
             bot.send_message(msg.chat.id, "How old are you?").await?;
             dialogue
-                .update(State::ReceiveAge {
-                    full_name: text.into(),
+                .update(State::RecieveStyle {
+                    language: text.into(),
                 })
                 .await?;
         }
@@ -88,18 +202,15 @@ async fn recieve_language(bot: Bot, dialogue: MyDialogue, msg: Message) -> Handl
     Ok(())
 }
 
-async fn receive_age(
+async fn receive_style(
     bot: Bot,
     dialogue: MyDialogue,
-    full_name: String, // Available from `State::ReceiveAge`.
+    language: String, // Available from `State::ReceiveAge`.
     msg: Message,
 ) -> HandlerResult {
     match msg.text().map(|text| text.parse::<u8>()) {
         Some(Ok(age)) => {
             bot.send_message(msg.chat.id, "What's your location?")
-                .await?;
-            dialogue
-                .update(State::ReceiveLocation { full_name, age })
                 .await?;
         }
         _ => {
@@ -129,11 +240,3 @@ async fn receive_location(
 
     Ok(())
 }
-
-/*
- * Структура бота:
- * Приветствие
- * Выбор языка,
- * Выбор стиля общения,
- *
- */
